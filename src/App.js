43eb872c8e5b6a0adde4cb6478a53aeb7b961542
Mogilnik Valeria <lera___2000-29@mail.ru>
2021-02-02 21:00:03 +1000

    mod

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-const name = 'Valeria';
-const age = 20;
-let sum = 5 + 8;
-let num;
-if (sum==13) num = true;
-let something = false;
+import count from './numbers.js';
+import length from './numbers.js';
 
 function App() {
   return (
@@ -15,30 +11,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style={{
-          color: 'green'
-        }}>
-          Hello world!!!!!!
-        </p>
+ 
         <p>
-         значение переменной name: {name}
+        {count*length}
         </p>
-        <p>
-         число: {age} 
-        </p>
-        <p>
-         результат арифметической операции: {sum}
-        </p>
-        <p>
-         результат логической операции: {num && 'true'}
-        </p>
-        <p>
-         тернарное условие: {something ? 'something is true' : 'something is false'}
-        </p>
-        <p>
-        {undefined} {null} {false} {true} 
-        </p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
