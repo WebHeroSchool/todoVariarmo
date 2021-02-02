c335a14ef2617225d353a78d9c48e31e46130a2c
Mogilnik Valeria <lera___2000-29@mail.ru>
2021-02-02 17:49:49 +1000

    jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Valeria';
+const age = 20;
+let sum = 5 + 8;
+let num;
+if (sum==13) num = true;
+let something = false;
+
 function App() {
   return (
     <div className="App">
@@ -9,8 +16,28 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <p style={{
+          color: 'green'
+        }}>
           Hello world!!!!!!
+        </p>
+        <p>
+         значение переменной name: {name}
+        </p>
+        <p>
+         число: {age} 
+        </p>
+        <p>
+         результат арифметической операции: {sum}
+        </p>
+        <p>
+         результат логической операции: {num && 'true'}
+        </p>
+        <p>
+         тернарное условие: {something ? 'something is true' : 'something is false'}
+        </p>
+        <p>
+        {undefined} {null} {false} {true} 
         </p>
         <a
           className="App-link"
