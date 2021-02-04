cfd0c96e720cc3c49e36e867085264d25332f21c
Mogilnik Valeria <54841241+Variarmo@users.noreply.github.com>
2021-02-04 19:12:06 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { element } from './App';
+import App from './App';
 
-ReactDOM.render( element,
+ReactDOM.render( <App />,
   document.getElementById('root')
 );
